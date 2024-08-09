ee178ac8124259eaf53b047b92f2eeacb3f2c01f
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2024-08-09 06:30:37 +0530

    Update slices.go

--- Basic/slices.go
+++ Basic/slices.go
@@ -12,6 +12,19 @@ func main() {
 	fruitList=append(fruitList,"Mango","Banana")
 	fmt.Println(fruitList);
 
-	fruitList=append(fruitList[1:3])
+	fruitList=append(fruitList[1:3]) // printing starts from 1 and stops at 3
 	fmt.Println(fruitList);
+	fruitList=append(fruitList[:3]) //printing all till 3
+
+	highScores:=make([]int,4) //make is a part of memory management which helps to create memory
+
+	highScores[0]=234
+	highScores[1]=945
+	highScores[2]=455
+	highScores[3]=889
+	//highScores[4]=888  //when we try to print this we will get an error, showing index out of range[4] with length4
+
+	highScores=append(highScores, 555,666,321) //but when we use append it does not show any error
+
+	fmt.Println(highScores)
 }
